1654417da4fa5d4c6d1254e2f96b9b572160fe8d
LiChen <xiaxinandye@163.com>
2019-08-31 12:15:41 +0800

    完成首页的topbar、searchbar布局

---
+++ components/public/header/geo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location">北京市</i>
+    <nuxt-link to="/changeCity" class="changeCity">
+      切换城市
+    </nuxt-link>
+    [香河 廊坊 天津]
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
